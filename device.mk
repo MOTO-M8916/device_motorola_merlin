Alberto97/android_device_motorola_osprey

Introduce a device configuration script to replace libinit

device.mk
@@ -40,6 +40,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Ramdisk
 PRODUCT_PACKAGES += \
+    init.osprey.config.sh \
     init.target.rc
 
 # Sensors

rootdir/Android.mk
@@ -1,5 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 
+# Configuration scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.osprey.config.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)

MOTO-M8916/device_motorola_merlin

device.mk
@@ -43,6 +43,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Ramdisk
 PRODUCT_PACKAGES += \
+    init.merlin.config.sh \
     init.target.rc
 
 

rootdir/Android.mk
@@ -1,5 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 
+# Configuration scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.merlin.config.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)