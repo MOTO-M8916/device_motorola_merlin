fa44f78a3f6bb27b881b1c2b9996a96ac05efb67
althafvly <althafvly@gmail.com>
2020-05-26 23:05:06 +0530

    merlin: mokee-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,5 +14,5 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_merlin.mk
+    $(LOCAL_DIR)/mokee_merlin.mk
 
--- mokee_merlin.mk
+++ mokee_merlin.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := lineage_merlin
+PRODUCT_NAME := mokee_merlin
 PRODUCT_MODEL := MotoG3-TE
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
